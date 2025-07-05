eefdf21f9f7e71d938d224fa470222610c4c5078
itsriyas <mohamedriyas.dev@gmail.com>
2025-07-05 11:23:35 +0530

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { FaEnvelope, FaPhoneAlt, FaFacebookF, FaTwitter, FaMapMarkerAlt } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 
@@ -12,32 +12,35 @@ const TopBar = () => (
     <div className="header-links d-flex gap-3">
       <a href=''><FaFacebookF /></a>
       <a href=''><FaTwitter /></a>
-      <a href='https://maps.app.goo.gl/HBDkaJ25SJ7fWPpR8' target='_blank'><FaMapMarkerAlt/></a>
+      <a href='https://maps.app.goo.gl/HBDkaJ25SJ7fWPpR8' target='_blank' rel="noopener noreferrer"><FaMapMarkerAlt/></a>
     </div>
   </div>
 );
 
 const MainNavbar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const closeMenu = () => setExpanded(false);
+
   return (
     <>
       <TopBar />
-      <Navbar expand="lg" bg="white" variant="light" className="border-bottom">
+      <Navbar expand="lg" bg="white" variant="light" className="border-bottom" expanded={expanded}>
         <Container>
           <Navbar.Brand href="/">
             <img src="assets/medigreen_logo.png" alt="Medigreen Hospital" height="60" />
           </Navbar.Brand>
-          <Navbar.Toggle />
+          <Navbar.Toggle onClick={() => setExpanded(!expanded)} />
           <Navbar.Collapse>
             <Nav className="ms-auto gap-md-3">
-              <NavLink to="/" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'}>Home</NavLink>
-<NavLink to="/about" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'}>About Us</NavLink>
-<NavLink to="/facilities" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'}>Facilities</NavLink>
-<NavLink to="/doctors" className={({ isActive }) => isActive ? 'nav-link text-success' : 'nav-link'}>Doctors</NavLink>
-<NavLink to="/news" className={({ isActive }) => isActive ? 'nav-link text-success' : 'nav-link'}>News</NavLink>
-<NavLink to="/career" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'}>Career</NavLink>
-<NavLink to="/gallery" className={({ isActive }) => isActive ? 'nav-link text-success' : 'nav-link'}>Gallery</NavLink>
-<NavLink to="/contact" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'}>Contact</NavLink>
-
+              <NavLink to="/" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'} onClick={closeMenu}>Home</NavLink>
+              <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'} onClick={closeMenu}>About Us</NavLink>
+              <NavLink to="/facilities" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'} onClick={closeMenu}>Facilities</NavLink>
+              <NavLink to="/doctors" className={({ isActive }) => isActive ? 'nav-link text-success' : 'nav-link'} onClick={closeMenu}>Doctors</NavLink>
+              <NavLink to="/news" className={({ isActive }) => isActive ? 'nav-link text-success' : 'nav-link'} onClick={closeMenu}>News</NavLink>
+              <NavLink to="/career" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'} onClick={closeMenu}>Career</NavLink>
+              <NavLink to="/gallery" className={({ isActive }) => isActive ? 'nav-link text-success' : 'nav-link'} onClick={closeMenu}>Gallery</NavLink>
+              <NavLink to="/contact" className={({ isActive }) => isActive ? 'nav-link text-success ' : 'nav-link'} onClick={closeMenu}>Contact</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
