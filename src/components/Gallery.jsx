d23de73a9860625aa6cdc1a11a0b0315b86c53e4
itsriyas <mohamedriyas.dev@gmail.com>
2025-07-05 11:15:25 +0530

    gallery updates

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -7,12 +7,12 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 const galleryImages = [
-  'https://avatars.mds.yandex.net/get-ydo/1817195/2a0000016b0be35138e5abfe01d780d4ea05/diploma',
-  'https://i.abcnewsfe.com/a/b50f5b4d-9512-4a34-bc8b-017a30339f85/hospital-room-rf-gty-ml-240329_1711713757738_hpMain.jpg',
-  'https://static.tildacdn.com/tild3331-3363-4563-b039-633961356466/1-52.jpg',
-  'https://static.tildacdn.com/tild6163-3063-4562-a463-383666356161/3248.jpg',
-  'https://i.ytimg.com/vi/aoqDt0XUBxU/maxresdefault.jpg',
-  'http://architizer-prod.imgix.net/media/1392745354741sm-010.jpg?w=1680&amp;q=60&amp;auto=format,compress&amp;cs=strip',
+  'assets/gallery/g2.jpg',
+  'assets/gallery/g8.jpg',
+  'assets/gallery/g9.jpg',
+  'assets/gallery/g4.jpg',
+  'assets/gallery/amb.jpg',
+  'assets/gallery/g14.jpg',
 ];
 
 const GallerySlider = () => {
